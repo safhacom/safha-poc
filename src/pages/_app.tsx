15922703b6471817a17479fdd0313070b5b3a308
Younes <hi@younes.ninja>
2023-12-02 23:50:58 +0300

    finalize: web golden sample

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
+import AppHead from "@/components/Head"
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
-import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Safha - Golden Website</title>
-        <link rel="shortcut icon" href="/icon.png" />
-      </Head>
+      <AppHead />
       <Component {...pageProps} />
     </>
   )
