52a34bd4887ddf8d423b3ef9cfe0d380db96eeb6
Faris Alahmadi <71413350+Faris-ML@users.noreply.github.com>
2023-12-06 11:33:12 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,6 @@
-import AboutSection from "@/components/AboutSection"
-import BlogSection from "@/components/BlogSection"
-import FooterSection from "@/components/FooterSection"
-import HeroSection from "@/components/HeroSection"
-import NavigationHeader from "@/components/NavigationHeader"
-import ProjectsSection from "@/components/ProjectsSection"
-
 export default function Home() {
   return (
     <>
-      <NavigationHeader />
-      <HeroSection backgroundImageSrc="/ai-background.png" />
-      <AboutSection
-        title="About Me"
-        description="I'm a professional developer with a passion for creating modern, elegant, and user-friendly applications."
-        imageUrl="/about.png"
-      />
-      <ProjectsSection />
-      <BlogSection />
-      <FooterSection />
     </>
   )
 }
