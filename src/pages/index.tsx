f4f0b1404e9a5b5d8ec48dd7877f69ff589cf681
Younes <hi@younes.ninja>
2023-12-15 01:49:41 +0300

    reset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
-import AboutSection from "@/components/AboutSection"
-import FooterSection from "@/components/FooterSection"
-import HeaderSection from "@/components/HeaderSection"
-import HeroSection from "@/components/HeroSection"
-import PricingSection from "@/components/PricingSection"
-
 export default function Home() {
-  return (
-    <>
-      <HeaderSection />
-      <HeroSection />
-      <AboutSection />
-      <PricingSection />
-      <FooterSection />
-    </>
-  )
+  return <>hello world</>
 }
